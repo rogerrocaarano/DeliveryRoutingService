ae4af8563be497a2ff8c92400bc0332247391c6d
Roger Roca Arano <git@rogerroca.me>
2025-05-19 18:12:39 -0400

    refactor: rename and restructure vehicle tracking endpoint methods

--- WebApi/Features/Vehicles/VehicleTracking/VehicleTrackingEndpoints.cs
+++ WebApi/Features/Vehicles/VehicleTracking/VehicleTrackingEndpoints.cs
@@ -1,9 +1,24 @@
+using MediatR;
+
 namespace WebApi.Features.Vehicles.VehicleTracking;
 
-public static class TrackingEndpoint
+public static class VehicleTrackingEndpoints
 {
-    public static void MapTrackingEndpoints(this WebApplication app)
+    private const string EndpointName = "vehicles/tracking";
+
+    public static void MapVehicleTrackingEndpoints(this WebApplication app)
     {
-        
+        app.MapGet($"/{EndpointName}/{{vehicleId:guid}}/positions", async (Guid vehicleId, ISender sender) =>
+        {
+            var positions = await sender.Send(new GetVehiclePositionsQuery(vehicleId));
+            return Results.Ok(positions);
+        });
+
+        app.MapPost($"/{EndpointName}/{{vehicleId:guid}}/positions", async (Guid vehicleId, AddVehiclePositionCommand command, ISender sender) =>
+        {
+            command = command with { VehicleId = vehicleId };
+            var id = await sender.Send(command);
+            return Results.Created($"/{EndpointName}/{vehicleId}/positions/{id}", id);
+        });
     }
 }
